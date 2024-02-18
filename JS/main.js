49aeeba328c9989c31092cc229d83faa38cf32da
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-02-17 23:26:21 -0300

    melhoras do menu

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var menuMagico = document.getElementById('menuMagico');
-    var magia = window.scrollY > 50 ? "blue" : "transparent"; // Azul se rolar mais de 50px
+    var magia = window.scrollY > 50 ? "#edebd8" : "transparent"; // Azul se rolar mais de 50px
 
     menuMagico.style.backgroundColor = magia;
 });
